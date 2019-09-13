fc93f40d6342f29d0c7265ab9262ee233fb27f32
masonkarsevar <masonkarsevar@gmail.com>
2019-09-13 09:51:29 -0700

    fixed get and post functionality for tasks

--- routes/tasksModel.js
+++ routes/tasksModel.js
@@ -1,12 +1,15 @@
 const db = require('../data/db-config.js');
 
 function getTasks() {
-    return db('tasks') 
+    return db('tasks')
+        .join('projects', 'tasks.project_id', '=', 'projects.id')
+        .select('tasks.id', 'tasks.description', 'tasks.completed', 'tasks.notes', 'projects.project_name') 
 }
 
 function getTasksByProjectId(project_id) {
     return db('tasks')
             .where({project_id})
+            .select('tasks.id', 'tasks.description', 'tasks.completed', 'tasks.notes') 
 }
 
 function postTask(taskPost) {
