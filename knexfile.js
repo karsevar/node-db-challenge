86e5bbb830695ddf03d21b28ceb15b4a84774a63
masonkarsevar <masonkarsevar@gmail.com>
2019-09-13 08:50:44 -0700

    database initialized

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './todo-list.db3'
+      filename: './data/todo-list.db3'
     },
     migrations: {
       directory: './data/migrations'
