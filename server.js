d0a6fd57898d983f73a4469b01c80eeeacb51471
masonkarsevar <masonkarsevar@gmail.com>
2019-09-13 08:32:15 -0700

    basic tasks route added

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const taskRoute = require('./routes/tasksRoute.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/tasks', taskRoute);
 
 server.use('/', (req, res) => {
     res.send(`<h2>Root Route is Printing something!!!</h2>`)
